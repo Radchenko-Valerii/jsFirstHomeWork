063e0f78f36d12cb4bb1cb1f2799da53f92538e7
Valerii <valeravivaz@gmail.com>
2021-07-19 12:38:14 +0300

    add decision of first two task

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+
+// 1. ФУНКЦИЯ ПРОВЕРКИ ВОЗРАСТА---------------------------------------
+
+// const isAdult = function(age){
+//   if (isNaN(age)){
+//     return null;
+//   }
+//   if (age<0){
+//     return null;
+//   }
+//   if (age>=18){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// 2. ФУНКЦИЯ ПРОВЕРКИ КРАТНОСТИ--------------------------------------
+
+// const checkMultiplicity = function(value1, value2){
+//   if (isNaN(value1) || isNaN(value2)){
+//     return null;
+//   }
+//   if (value1<=0 || value2<=0){
+//     return null;
+//   }
+//   if (value1 % value2 === 0){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
