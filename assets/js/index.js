787049b3cd56990095c36993e0338a87abb990e7
Valerii Radchenko <valeravivaz@gmail.com>
2021-07-19 14:18:02 +0300

    add last part of fourth

--- assets/js/index.js
+++ assets/js/index.js
@@ -77,5 +77,37 @@
 //   return areaCylinder;
 // }
 
+// 4.3 Треугольник --------------------
+
+// let areaTriangle;
+// let p;
+// const calculationAreaTriangle = function(length1, length2, length3){
+//   if (isNaN(length1) || isNaN(length2) || isNaN(length3)){
+//     return areaTriangle=null;
+//   }
+//   if (length1<=0 || length2<=0 || length3<=0){
+//     return areaTriangle=null;
+//   }
+//   if (length1 + length2 < length3 || length3 + length2 < length1 || length1 + length3 < length2){
+//     return areaTriangle=null;
+//   }
+//   p=(length1+length2+length3)/2;
+//   areaTriangle = Math.sqrt(p*(p-length1)*(p-length2)*(p-length3));
+//   return areaTriangle;
+// }
+
+// 4.4 Прямоугольник -----------------
+
+// let areaRectangle;
+// const calculationAreaRectangle = function(length1, length2){
+//   if (isNaN(length1) || isNaN(length2)){
+//     return areaRectangle=null;
+//   }
+//   if (length1<=0 || length1<=0){
+//     return areaRectangle=null;
+//   }
+//   areaRectangle = length1*length2;
+//   return areaRectangle;
+// }
 
 
