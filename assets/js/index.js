f9bb071e6636a8b958fee8390f6df7b637481576
Valerii <valeravivaz@gmail.com>
2021-07-19 12:49:43 +0300

    add third task

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,3 +30,19 @@
 //     return false;
 //   }
 // }
+
+// 3. ФУНКЦИЯ ПРОВЕРКИ ВОЗМОЖНОСТИ СОЗДАНИЯ ТРЕУГОЛЬНИКА
+
+// const checkTrianglePossibility = function(length1, length2, length3){
+//   if (isNaN(length1) || isNaN(length2) || isNaN(length3)){
+//     return null;
+//   }
+//   if (length1<=0 || length2<=0 || length3<=0){
+//     return null;
+//   }
+//   if (length1 + length2 > length3 && length3 + length2 > length1 && length1 + length3 > length2){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
