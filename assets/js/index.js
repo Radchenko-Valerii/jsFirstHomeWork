cbffa5d568198a33d721460f3cbf23229da882e3
Valerii Radchenko <valeravivaz@gmail.com>
2021-08-04 15:57:12 +0300

    fix something

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,3 @@
-
 // 1. ФУНКЦИЯ ПРОВЕРКИ ВОЗРАСТА---------------------------------------
 
 // const isAdult = function(age){
@@ -51,65 +50,58 @@
 
 // 4.1 Ромб----------------------
 
-// let areaRhombus;
 // const calculationAreaRhombus = function(diagonal1, diagonal2){
 //   if (isNaN(diagonal1) || isNaN(diagonal2)){
-//     return areaRhombus=null;
+//     return null;
 //   }
 //   if (diagonal2<=0 || diagonal1<=0){
-//     return areaRhombus=null;
+//     return null;
 //   }
-//   areaRhombus = 0.5*diagonal1*diagonal2;
-//   return areaRhombus;
+//   return 0.5*diagonal1*diagonal2;
+//
 // }
 
 // 4.2 Цилиндр -------------------
 
-// let areaCylinder;
-// const calculationAreaRhombus = function(radius, height){
+//
+// const calculationAreaCylinder = function(radius, height){
 //   if (isNaN(radius) || isNaN(height)){
-//     return areaCylinder=null;
+//     return null;
 //   }
 //   if (radius<=0 || height<=0){
-//     return areaCylinder=null;
+//     return null;
 //   }
-//   areaCylinder = 2*3.14*radius*(radius+height);
-//   return areaCylinder;
+//   return 2*3.14*radius*(radius+height);
 // }
 
 // 4.3 Треугольник --------------------
 
-// let areaTriangle;
-// let p;
 // const calculationAreaTriangle = function(length1, length2, length3){
 //   if (isNaN(length1) || isNaN(length2) || isNaN(length3)){
-//     return areaTriangle=null;
+//     return null;
 //   }
 //   if (length1<=0 || length2<=0 || length3<=0){
-//     return areaTriangle=null;
+//     return null;
 //   }
 //   if (length1 + length2 < length3 || length3 + length2 < length1 || length1 + length3 < length2){
-//     return areaTriangle=null;
+//     return null;
 //   }
-//   p=(length1+length2+length3)/2;
-//   areaTriangle = Math.sqrt(p*(p-length1)*(p-length2)*(p-length3));
-//   return areaTriangle;
+//   let p=(length1+length2+length3)/2;
+//   return Math.sqrt(p*(p-length1)*(p-length2)*(p-length3));
+//
 // }
 
 // 4.4 Прямоугольник -----------------
 
-// let areaRectangle;
 // const calculationAreaRectangle = function(length1, length2){
 //   if (isNaN(length1) || isNaN(length2)){
-//     return areaRectangle=null;
+//     return null;
 //   }
 //   if (length1<=0 || length1<=0){
-//     return areaRectangle=null;
+//     return null;
 //   }
-//   areaRectangle = length1*length2;
-//   return areaRectangle;
+//   return length1*length2;
 // }
-
 
 // *** ВОЗВЕДЕНИЕ В СТЕПЕНЬ------------------------------------------
 
@@ -120,7 +112,7 @@
 //   if (degree === 0){
 //     return 1;
 //   }
-//   if (degree >= 1){
+//   if (degree > 0){
 //     let result = 1;
 //     for(let i=0; i < degree; i++){
 //       result *= number;
